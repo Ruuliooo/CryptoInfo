17903ad125eadd3f670aad4cb8aa4779202c7633
Ruuliooo <jules.suire.pro@gmail.com>
2025-03-19 10:49:58 +0100

    Update

---
+++ assets/pages/index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COINGECKO CHECKER</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/styles/style.css">
+</head>
+<body>
+  <header>
+    <div class="header-container">
+      <img class="fit-picture" src="../img/logo_crypto.png" alt="Logo Crypto" />
+      <nav>
+      </nav>
+    </div>
+  </header>
+  <main> 
